1b2cb78c14ce92e73323ecbd5dfb7658469d73d3
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-11-11 22:46:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,5 @@
-# bot.py
-import os
 from pyrogram import Client, idle
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
-
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_DB_URI = os.getenv("MONGO_DB_URI", "")
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID
 
 # Plugins folder
 plugins = dict(root="plugins")
